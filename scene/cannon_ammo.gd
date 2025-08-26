1d03fa8a55b099af7f6cfc1684b06868cf9671d0
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2025-08-26 20:52:15 +0800

    added logic for ammo explosion

--- scene/cannon_ammo.gd
+++ scene/cannon_ammo.gd
@@ -2,12 +2,22 @@ extends Area2D
 
 @export var speed = 300
 var direction: String
+@onready var animated_sprite = $AnimatedSprite2D
 func _physics_process(delta: float) -> void:
-	if direction == "left":
+	if direction == "left" and not animated_sprite.animation == "destroyed":
 		position.x -= speed * delta
-	elif direction == "right":
+	elif direction == "right" and not animated_sprite.animation == "destroyed":
 		position.x += speed * delta
 
+func _explode():
+	animated_sprite.animation = "destroyed"
+
+func _on_animation_finished():
+	if animated_sprite.animation == "destroyed":
+		queue_free()
 
 func _on_despawn_timer_timeout() -> void:
 	queue_free()
+
+func _on_body_entered(body: Node2D) -> void:
+	_explode()
