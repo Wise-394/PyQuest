fc29e0da380afa466f879669866b4cffb23faec9
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2025-12-05 20:55:25 +0800

    added opening dialog and its logic

--- script/starfish/starfish.gd
+++ script/starfish/starfish.gd
@@ -1,28 +1,37 @@
 extends Area2D
 
 @onready var label: Label = $Label
-var isVisible = false
+var _isVisible = false
+var _isDialogActive = false
 
+func _ready() -> void:
+	DialogueManager.dialogue_ended.connect(_on_dialog_finished)
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("open_dialog") and isVisible:
-		open_dialog()
+	if Input.is_action_just_pressed("open_dialog") and _isVisible and not _isDialogActive:
+		_open_dialog()
+	
 			
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		changeVisibility()
+		_change_visibility()
 
 
 func _on_body_exited(body: Node2D) -> void:
 		if body.name == "Player":
-			changeVisibility()
+			_change_visibility()
 
-func changeVisibility():
-	if isVisible == true:
+func _change_visibility():
+	if _isVisible == true:
 		label.visible = false
-		isVisible = false
+		_isVisible = false
 	else:
 		label.visible = true
-		isVisible = true
+		_isVisible = true
 
-func open_dialog():
-	print("dialog open")
+func _open_dialog():
+	var resource = load("res://dialog/introduction.dialogue")
+	DialogueManager.show_dialogue_balloon(resource, "start")
+	_isDialogActive = true
+	
+func _on_dialog_finished(_resource):
+	_isDialogActive = false
