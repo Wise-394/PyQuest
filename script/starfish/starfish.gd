aebb7c34024557262ce9c8c525d8eaa98123cdb9
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2025-12-05 20:41:41 +0800

    added starfish function open dialog

--- script/starfish/starfish.gd
+++ script/starfish/starfish.gd
@@ -1,11 +1,28 @@
 extends Area2D
 
 @onready var label: Label = $Label
+var isVisible = false
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("open_dialog") and isVisible:
+		open_dialog()
+			
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		label.visible	= true
+		changeVisibility()
 
 
 func _on_body_exited(body: Node2D) -> void:
 		if body.name == "Player":
-			label.visible = false	
+			changeVisibility()
+
+func changeVisibility():
+	if isVisible == true:
+		label.visible = false
+		isVisible = false
+	else:
+		label.visible = true
+		isVisible = true
+
+func open_dialog():
+	print("dialog open")
