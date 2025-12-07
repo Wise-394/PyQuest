3820512c3ef1cd8d723c88798e04d5fac11ed2da
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2025-12-07 21:23:40 +0800

    fixed bug of movement when opening console

--- script/player/code_edit_state.gd
+++ script/player/code_edit_state.gd
@@ -4,7 +4,8 @@ class_name CodeEditState
 func enter():
 	init_references()
 	sprite.play("idle")
-
+	character.velocity.x = 0
+	
 func update(delta) -> void:
 	character.velocity.y += character.gravity * delta
 	character.move_and_slide()
--- script/player/dialog_state.gd
+++ script/player/dialog_state.gd
@@ -4,11 +4,12 @@ class_name DialogState
 func enter():
 	init_references()
 	sprite.play("idle")
+	character.velocity.x = 0
 	
 func update(delta) -> void:
 	character.velocity.y += character.gravity * delta
 	character.move_and_slide()
-	
+
 func physics_update(_delta):
 	pass
 
