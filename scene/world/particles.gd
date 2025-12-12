f8011d1df86bf845c27c1486bdc98c42958265bb
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2025-12-12 15:26:19 +0800

    Add animated particles to coding zone and level 1
    Introduced new particle textures and a CPUParticles2D node with animation and proximity-based emission to the coding zone and level 1 scenes. Added a custom particles.gd script to control particle emission based on player distance. Updated scene layouts and collision shapes to accommodate the new effects.

---
+++ scene/world/particles.gd
@@ -0,0 +1,12 @@
+extends CPUParticles2D
+
+@export var max_distance := 300.0
+@onready var player := get_tree().current_scene.get_node("Player")
+
+func _process(_delta):
+	var dist = global_position.distance_to(player.global_position)
+
+	if dist <= max_distance:
+		emitting = true
+	else:
+		emitting = false
