5e6f0e402221a250e52458db5e24080e4901fbdc
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2025-12-15 21:47:54 +0800

    Add level 2 and enable level transitions via flag
    Added lvl_2.tscn as a new level. Updated player.tscn to set a default speed. Modified lvl_1.tscn and flag.gd to support transitioning to the next level when all zones are completed, using a new 'level_to_go' property on the flag.

--- script/world/flag.gd
+++ script/world/flag.gd
@@ -1,15 +1,21 @@
 extends Area2D
 
-
 @onready var zone_remaining: Label = get_tree().current_scene.get_node("UI/CanvasLayer/ZoneRemaining")
+
+@export_file("*.tscn") var level_to_go: String
+
 func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
-		if(zone_remaining.get_not_completed_count() <= 0):
-			end_level()
-		else:
-			print("not completed")
-		
+	if body.name != "Player":
+		return
 
+	if zone_remaining.get_not_completed_count() <= 0:
+		call_deferred("next_level")
+	else:
+		print("not completed")
 
-func end_level():
-	print("everything finished")
+func next_level():
+	if level_to_go == "":
+		push_error("Next level not set!")
+		return
+
+	get_tree().change_scene_to_file(level_to_go)
