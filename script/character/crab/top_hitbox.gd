b066fbcf6079cdd130b9280df9bf7d2b2da4af9e
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2025-12-28 21:05:06 +0800

    Add cannon and sign to level 5, update platforms
    Added cannon and sign scene resources to lvl_5.tscn and updated the load steps accordingly. Removed unused properties from moving platforms in lvl_1.tscn. No logic changes, only scene and resource updates.

--- script/character/crab/top_hitbox.gd
+++ script/character/crab/top_hitbox.gd
@@ -4,5 +4,5 @@ extends Area2D
 @onready var character = $".."
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
+	if body.name == "Player" and character.is_alive:
 		body.character_damaged(character.damage, self)
