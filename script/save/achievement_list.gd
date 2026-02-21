d8940c04a0d5160e76098ca3a475eaffb2361242
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2026-02-21 15:26:16 +0800

    Add AchievementList scene and script
    Introduce AchievementList Node and script to centralize achievement definitions. Adds scene (scene/save/achievement_list.tscn), GDScript class AchievementList (script/save/achievement_list.gd) with an exported Array of Dictionary entries for several achievements (hand_midas_1..3, herald, crusader, Immortal), and the script UID file (script/save/achievement_list.gd.uid). This allows the achievement data to be edited in the Godot editor and reused across the project.

---
+++ script/save/achievement_list.gd
@@ -0,0 +1,35 @@
+extends Node
+class_name AchievementList
+
+@export var achievements: Array[Dictionary] = [
+	{
+		"id": "hand_midas_1",
+		"title": "Hand of Midas I",
+		"description": "Gain 100 gold",
+	},
+	{
+		"id": "hand_midas_2",
+		"title": "Hand of Midas II",
+		"description": "Gain 200 gold",
+	},
+	{
+		"id": "hand_midas_3",
+		"title": "Hand of Midas III",
+		"description": "Gain 300 gold",
+	},
+	{
+		"id": "herald",
+		"title": "Herald",
+		"description": "finish level 1",
+	},
+	{
+		"id": "crusader",
+		"title": "Crusader",
+		"description": "finish level 10",
+	},
+	{
+		"id": "Immortal",
+		"title": "Immortal",
+		"description": "finish level 20",	
+	}
+]
